8accc8dcdffb6c41b12f230180ec41f7e3ccff7a
Anton Lo <antshot19@gmail.com>
2019-04-05 20:01:30 +0300

    Выполненное задание 3.

--- src/lesson_3/Task_3.java
+++ src/lesson_3/Task_3.java
@@ -7,12 +7,16 @@ import java.util.Scanner;
 import javax.annotation.processing.SupportedOptions;
 
 public class Task_3 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         guessNumber();
+        guessWord();
+
+
+
 
     }
-    static void guessNumber(){
+
+    static void guessNumber() {
         Random rnd = new Random();
         int unknown;
         int continueGame;
@@ -32,7 +36,7 @@ public class Task_3 {
                 count++;
                 if (number < unknown) System.out.println("Загаданное число больше.");
                 else if (number > unknown) System.out.println("Згаданное число меньше.");
-                    else System.out.println("Победа!!!");
+                else System.out.println("Победа!!!");
             } while (unknown != number && count < 3);
             System.out.println("повторить игру еще раз");
             System.out.println("1 - да / 0 - нет");
@@ -43,5 +47,51 @@ public class Task_3 {
         System.out.println("Досвидания");
     }
 
+    static void guessWord() {
+        String userWord;
+        String serchWord;
+        Scanner sc = new Scanner(System.in);
+        serchWord = randomWord();
+        char[] resWord = new char[15];
+        for (int i = 0; i < 15; i++) resWord[i] = '#';
 
+        //System.out.println(serchWord);  // Вывод загаданного слова.
+        System.out.println("Угадай слово.");
+
+        do {
+            System.out.println("Напиши свой ответ: ");
+            userWord = sc.nextLine();
+            if (!userWord.equals(serchWord)) {
+                System.out.println("Неверное слово!");
+                System.out.println(Arrays.toString(wordAnalogLetter(userWord, serchWord, resWord)));
+            }
+
+        } while (!userWord.equals(serchWord));
+        System.out.println("Угадал негодник!!!");
+
+        }
+    static String randomWord() {
+    Random rnd = new Random();
+    String word;
+    String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+            "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+            "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+    int wordNum = rnd.nextInt(words.length);
+    return word = words[wordNum];
+
+    }
+    static char[] wordAnalogLetter(String userWord, String serchWord,char[] resultWord){
+        int minWordLength;
+        if (serchWord.length() < userWord.length() ) minWordLength = serchWord.length();
+            else minWordLength = userWord.length();
+
+        for (int i = 0; i < minWordLength ; i++) {
+            if (serchWord.charAt(i) == userWord.charAt(i))
+                    resultWord[i] = serchWord.charAt(i);
+
+            }
+        //System.out.println(Arrays.toString(resWord));
+        return resultWord;
+    }
 }
