d422a369a6741dd4caf7106b22761372b686c2aa
Anton Lo <antshot19@gmail.com>
2019-04-11 23:42:34 +0300

    Task_5 Workers.

---
+++ src/Lesson_5/Lesson_5.java
@@ -0,0 +1,35 @@
+package Lesson_5;
+
+import java.awt.Color;
+
+public class Lesson_5 {
+    public static void main(String[] args){
+        Cat cat1 = new Cat();
+        cat1.name = "Vasca";
+        cat1.color = "red";
+        cat1.age = 10;
+
+        Cat cat2 = new Cat();
+        cat2.name = "Barsic";
+        cat2.color = "green";
+        cat2.age = 5;
+
+        cat1.info();
+        cat2.info();
+    }
+}
+class Cat{
+    String name;
+    String color;
+    int age;
+
+    public Cat(String name) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.println(name + " " + color + " " + age);
+    }
+}
