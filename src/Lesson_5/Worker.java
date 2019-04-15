cdb2b647e13bff172e52f623d905df6a83be3d8c
Anton Lo <antshot19@gmail.com>
2019-04-15 14:19:54 +0300

    Task_5 Workers. Worker - класс с полями( ФИО, должность, email, телефон, зарплата, возраст), конструктором и методом info(выводит информацию об объекте в консоль).
    persArray - массив сотрудников(5 шт.).
    olderThen40 - метод с помощью цикла выводящий информацию только о сотрудниках старше 40 лет.

--- src/Lesson_5/Worker.java
+++ src/Lesson_5/Worker.java
@@ -6,7 +6,7 @@ public class Worker {
         String position;
         String e_mail;
         String tel_num;
-        int slary;
+        int salary;
         int age;
 
         public Worker(String name, String position, String e_mail, String tel_num, int slary, int age) {
@@ -14,7 +14,7 @@ public class Worker {
             this.position = position;
             this.e_mail = e_mail;
             this.tel_num = tel_num;
-            this.slary = slary;
+            this.salary = slary;
             this.age = age;
         }
 
@@ -23,7 +23,7 @@ public class Worker {
             System.out.println("Position: " + position);
             System.out.println("E-Mail:   " + e_mail);
             System.out.println("Tel:      " + tel_num);
-            System.out.println("Salary:   " + slary + " USD");
+            System.out.println("Salary:   " + salary + " USD");
             System.out.println("Age:      " + age + " e.o.");
             System.out.println();
         }
