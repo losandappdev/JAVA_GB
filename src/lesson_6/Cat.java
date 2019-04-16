75355ced0c4d58a2664bca2849b15492d2a5e3d7
Anton Lo <antshot19@gmail.com>
2019-04-16 23:58:13 +0300

    Task 6. Kлассы: Animal, Dog, Cet, MainAnimal. Конструкторы. Методы.

--- src/lesson_6/Cat.java
+++ src/lesson_6/Cat.java
@@ -1,6 +1,8 @@
 package lesson_6;
 
- class Cat extends Enimal {
+  class Cat extends Enimal {
+
+
 
     private Cat(String name, String breed, int runLim, int jumpLim) {
         super(name, breed, runLim, jumpLim);
--- src/lesson_6/Dog.java
+++ src/lesson_6/Dog.java
@@ -2,9 +2,12 @@ package lesson_6;
 
  class Dog extends Enimal {
 
-     private Dog(String name, String breed, int runLim, int swimLim, int jumpLim) {
+
+
+    private Dog(String name, String breed, int runLim, int swimLim, int jumpLim) {
         super(name, breed, runLim, swimLim, jumpLim);
     }
+
 
     void voice() {
         System.out.println("Gav");
